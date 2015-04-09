5ddd5ce6beed4d6ba4661bbd91c1532bccbc22c0
jaygon4146 <jaygon4146@gmail.com>
2015-04-09 18:21:48 +1000

    added 'count down' feature to wind is this how I new branch?
    Signed-off-by: jaygon4146 <jaygon4146@gmail.com>

--- Assets/Scripts/wind.cs
+++ Assets/Scripts/wind.cs
@@ -2,61 +2,102 @@
 using System.Collections;
 //#include <math.h>
 
-public class wind : MonoBehaviour {
+public class wind : MonoBehaviour 
+{
 	public GUIText output;
 
-	private int angle;
-	private float speed;
+	private int 	angle;
+	private float	currWSpeed;
+	private float 	maxWSpeed;
+	private float 	speedInc;
+	private bool 	countDown;
+	private Vector3 windDir;
 
 	
 	// Use this for initialization
-	void Start () {
-		InvokeRepeating("updateWind", 1f, 1f);
+	void Start () 
+	{
+		beginWave(100f);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{	
 	}
-	
-	void FixedUpdate() {
-	
-	}
-	
-	void updateWind() {
-		angle = Random.Range(0, 360);
 
-		speed = Random.Range(0f, 1f);
+	//Called once per physics step
+	void FixedUpdate() 
+	{	
+		if (countDown) 
+		{
+			if (currWSpeed >= maxWSpeed)		//if reached max spped
+			{
+				currWSpeed = 0;
+				countDown = false;				//stop countDown
+			}
+			else 								//otherwise must still be ticking			
+			{
+				currWSpeed += speedInc;			//increase speed
+			}
+		}
+	}
+
+	void beginWave(float s)
+	{
+		setWind (s);
+		
+		speedInc = maxWSpeed / 1000;
+		currWSpeed = 0;
+		countDown = true;
+
+		InvokeRepeating ("updateWind", 1f, 1f);//updatewind
+	}
+
+	void setWind(float s)
+	{
+		angle = Random.Range(0, 360);		
+		//maxSpeed = Random.Range(0f, 10f);//get from wave timer function
+		maxWSpeed = s;//get from wave timer function
+	}
+	
+	void updateWind() 
+	{
 		/*
 		Speed begins at 0. Ticks towards 1 over course of the wave (30 secs) then returns to 0 rapidly (not instantaneously)
 		Speed slowly winds up before dying down
 		Top speed is determined by a multiplier dependant on current wave difficulty.
 		 */
 
-		//Change tag to "Blowable" ???
-		foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Gold")) {
-			if(obj.constantForce !=null) {
+		//if (countDown) 
+		//{
+			//Change tag to "Blowable" ???
+			foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Gold")) {
+				if (obj.constantForce != null) {
 
-				Vector3 wind = obj.constantForce.force;
+					windDir = obj.constantForce.force;
 
-				wind.x = Mathf.Cos(angle);
-				wind.z = Mathf.Sin(angle); 
+					windDir.x = (Mathf.Cos (angle) * currWSpeed);
+					windDir.z = (Mathf.Sin (angle) * currWSpeed); 
 					
-				obj.constantForce.force = wind;
-				output.text ="Wind Direction X: " + wind.x.ToString() +", Z: " + wind.z.ToString() + ", Speed: " + speed;
-
+					obj.constantForce.force = windDir;
+				}
 			}
-		}
 
 
-		//Add the wind to a cloth (flag)
-		GameObject flagObj = GameObject.FindGameObjectWithTag ("Flag");
-		Vector3 flagVec = flagObj.transform.GetComponent<Cloth> ().externalAcceleration;
-		flagVec.x += (speed * 2) * Mathf.Cos(angle);
-		flagVec.z += (speed * 2) * Mathf.Sin(angle);
-		flagObj.transform.GetComponent<Cloth> ().externalAcceleration = flagVec;
+			//Add the wind to a cloth (flag)
+			GameObject flagObj = GameObject.FindGameObjectWithTag ("Flag");
+			Vector3 flagVec = flagObj.transform.GetComponent<Cloth> ().externalAcceleration;
+			flagVec.x =  (Mathf.Cos (angle) * currWSpeed)*0.5f;
+			flagVec.z = (Mathf.Sin (angle) * currWSpeed)*0.5f; 
+			flagObj.transform.GetComponent<Cloth> ().externalAcceleration = flagVec;
+		//}
 
+		output.text = "Wind Direction X: " + windDir.x.ToString () + ", Z: " + windDir.z.ToString () + ", Speed: " + currWSpeed + " / " + maxWSpeed;
+		
 	}
+
+		
+
 
 
 
