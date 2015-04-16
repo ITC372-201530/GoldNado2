479ff3fb2ccdb19a0329db833af75a61849a0944
Flunkus <flunkus_13@hotmail.com>
2015-04-16 17:22:49 +1000

    Added crosshairs and buggy ammo pickups (stop spawning after a while). Damage is implemented and the playerController script has been changed so that the player cannot move in the air.

---
+++ Assets/Scripts/Shooting Scripts/ammoObjectScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ammoObjectScript : MonoBehaviour {
+
+
+	public float spinSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (Vector3.up, spinSpeed * Time.deltaTime);
+	
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.tag == "Player") {
+			gameVariables.ammunition += 2;
+			this.gameObject.SetActive(false);
+			gameVariables.currentPickups--;
+		}
+	}
+}
