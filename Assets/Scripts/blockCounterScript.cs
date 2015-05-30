d68b15ba1d532e0db2651b64ca57c373b49e0069
hyperant <toja.design@gmail.com>
2015-05-30 10:40:00 +1000

    add start screen, fixed gold pickup prefab to use FBX
    Signed-off-by: hyperant <toja.design@gmail.com>

--- Assets/Scripts/blockCounterScript.cs
+++ Assets/Scripts/blockCounterScript.cs
@@ -31,7 +31,6 @@ public class blockCounterScript : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/startScreen/mainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void OnClickPlay(){
+		Application.LoadLevel("goldNado");
+	}
+}
